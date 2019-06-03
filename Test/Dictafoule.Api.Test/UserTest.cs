16ceb81b3468417ad5138563a502659c82252e20
GuillaumeVerret <verret.guillaume@outlook.com>
2019-06-03 23:37:46 +0200

    Ajout du projet de test et du premier test User

---
+++ Test/Dictafoule.Api.Test/UserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using DictaFoule.API.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dictafoule.Api.Test
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        [Description("Ceci test est un pour recuperer l'utilisateur")]
+        public void GetUserTestShouldReturnNotFoundResult()
+        {
+            var controller = new UserController();
+            var reponse = controller.GetUser("tutu");
+            Assert.IsInstanceOfType(reponse, typeof(NotFoundResult));
+        }
+    }
+}
