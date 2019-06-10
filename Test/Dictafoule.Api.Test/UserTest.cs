120d271d1e948a17c537d5d6b8f69b1bcf1200c1
Guillaume Verret <starswarsdu92@hotmail.fr>
2019-06-10 11:46:38 +0200

    Modification du publish profil et ajout de nouveau test

--- Test/Dictafoule.Api.Test/UserTest.cs
+++ Test/Dictafoule.Api.Test/UserTest.cs
@@ -1,22 +1,51 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 using System.Web.Http.Results;
 using DictaFoule.API.Controllers;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DictaFoule.API.Models.Project;
+using DictaFoule.Common.DAL;
+using NUnit.Framework;
 
 namespace Dictafoule.Api.Test
 {
-    [TestClass]
+    [TestFixture]
     public class UserTest
     {
-        [TestMethod]
+        private UserController userController { get; set; }
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            userController = new UserController();
+        }
+
+        [Test]
+        public void CreateUserShouldReturnOK()
+        {
+            var userModel = new UserModel()
+            {
+                Guid = "1a0c42b8-5465-4dbe-8660-45e22d8d38cd"
+            };
+            var response = userController.CreateUser(userModel);
+            Assert.IsInstanceOf<OkResult>(response, "La reponse n'est pas du type OkResult");
+        }
+
+        [Test]
         [Description("Ceci test est un pour recuperer l'utilisateur")]
         public void GetUserTestShouldReturnNotFoundResult()
         {
-            var controller = new UserController();
-            var reponse = controller.GetUser("tutu");
-            Assert.IsInstanceOfType(reponse, typeof(NotFoundResult));
+            var reponse = userController.GetUser("titi");
+            Assert.IsInstanceOf<NotFoundResult>(reponse);
+        }
+
+        [Test]
+        [Description("Ceci test est un pour recuperer l'utilisateur")]
+        public void GetUserTestShouldReturnFound()
+        {
+            var reponse = userController.GetUser("1a0c42b8-5465-4dbe-8660-45e22d8d38cd");
+            Assert.IsInstanceOf<OkNegotiatedContentResult<user>>(reponse, "La reponse n'est pas du type OkResult");
         }
     }
 }
