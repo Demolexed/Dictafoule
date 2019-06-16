2ef6cfd8f84b0423141da9297de98dcce8a2bbef
GuillaumeVerret <verret.guillaume@outlook.com>
2019-06-16 19:20:12 +0200

    Ajout d'un healthChecking pour le serveur db

---
+++ DictaFoule.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DictaFoule.API.Controllers
+{
+    public class HealthCheckController : ApiController
+    {
+        [HttpGet]
+        [Route("v1/Project/HealthChecking")]
+        public IHttpActionResult HealthChecking()
+        {
+            return Ok();
+        }
+    }
+}
