3c6358645019b5717a10f88dd0f6e48da1329784
Guillaume Verret <verret.guillaume@outlook.com>
2019-06-23 23:37:57 +0200

    Auto stash before rebase of "origin/developp"

---
+++ DictaFoule.Mobile/Dictafoule.Mobile.IOS.Test/UserTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DictaFoule.Mobile.iOS.Business;
+using NUnit.Framework;
+namespace Dictafoule.Mobile.IOS.Test
+{
+    [TestFixture]
+    public class UserTest
+    {
+        [Test]
+        public void TestUserCreateUser()
+        {
+            var user = new User();
+            Assert.DoesNotThrow(() => user.CreateUser());
+            Assert.DoesNotThrow(() => user.GetUser());
+        }
+    }
+}
